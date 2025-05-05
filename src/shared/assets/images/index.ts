d6686c8db61963e8f7fc4ec7b75106e0f2a4e56b
Jinviz <rlawlsdn9583@gmail.com>
2025-05-05 21:47:25 +0900

    Chore: 하단 네비게이션 아이콘 업로드

--- src/shared/assets/images/index.ts
+++ src/shared/assets/images/index.ts
@@ -1,11 +1,18 @@
 export { default as bg_group1 } from "./bg_group1.png";
 export { default as bg_group2 } from "./bg_group2.webp";
 export { default as nemo_logo } from "./nemo_logo.svg";
+// 하단 네비게이션 바
 export { default as home_icon } from "./home_icon.svg";
 export { default as magnifying_glass_icon } from "./magnifying_glass_icon.svg";
 export { default as ai_icon } from "./ai_icon.svg";
 export { default as dashboard_icon } from "./dashboard_icon.svg";
-export { default as user_icon } from "./user_icon.svg";
+export { default as profile_icon } from "./profile_icon.svg";
+export { default as home_active_icon } from "./home_active_icon.svg";
+export { default as magnifying_glass_active_icon } from "./magnifying_glass_active_icon.svg";
+export { default as ai_active_icon } from "./ai_active_icon.svg";
+export { default as dashboard_active_icon } from "./dashboard_active_icon.svg";
+export { default as profile_active_icon } from "./profile_active_icon.svg";
+// 아이콘
 export { default as users_icon } from "./users_icon.svg";
 export { default as users_bk_icon } from "./users_bk_icon.svg";
 export { default as bell_icon } from "./bell_icon.svg";
