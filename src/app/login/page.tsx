201d92f721e9451eeb43576cf9d88a2b0ad13731
Jinviz <rlawlsdn9583@gmail.com>
2025-05-15 14:59:40 +0900

    Fix: 클라이언트 컴포넌트 페이지 Suspense 바운더리 처리

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,7 +6,7 @@ import { useAuthStore } from "@/shared/store/auth-store";
 import Image from "next/image";
 import { useSearchParams } from "next/navigation";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { Suspense, useEffect } from "react";
 
 export default function LoginPage() {
   const router = useRouter();
@@ -22,23 +22,31 @@ export default function LoginPage() {
   }, [token, router, login]);
 
   return (
-    <div className="p-ctn-lg flex min-h-screen flex-col items-center justify-center">
-      <h1 className="text-title-1 text-label-strong-2 text-right font-extralight">
-        네가 찾는 모임,
-        <br />
-        <span className="text-display-1 text-primary-strong font-bold">
-          네모!
-        </span>
-      </h1>
-      {/* <Image src={nemo_logo} alt="Logo" width={192} height={192} /> */}
-      <Image
-        src={bg_post}
-        alt="배경 이미지"
-        width={318}
-        height={318}
-        className="mt-10 ml-15"
-      />
-      <LoginButton platform="kakao" />
-    </div>
+    <Suspense
+      fallback={
+        <div className="flex h-24 items-center justify-center">
+          <div className="border-primary h-6 w-6 animate-spin rounded-full border-2 border-t-transparent"></div>
+        </div>
+      }
+    >
+      <div className="p-ctn-lg flex min-h-screen flex-col items-center justify-center">
+        <h1 className="text-title-1 text-label-strong-2 text-right font-extralight">
+          네가 찾는 모임,
+          <br />
+          <span className="text-display-1 text-primary-strong font-bold">
+            네모!
+          </span>
+        </h1>
+        {/* <Image src={nemo_logo} alt="Logo" width={192} height={192} /> */}
+        <Image
+          src={bg_post}
+          alt="배경 이미지"
+          width={318}
+          height={318}
+          className="mt-10 ml-15"
+        />
+        <LoginButton platform="kakao" />
+      </div>
+    </Suspense>
   );
 }
