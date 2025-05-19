57693834ae79a4f060e9a6b8a1bd719ea0e03081
Jinviz <rlawlsdn9583@gmail.com>
2025-05-19 10:53:30 +0900

    Chore: 카카오 리다이렉트 주소 상수화

--- src/shared/constants/url.ts
+++ src/shared/constants/url.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
-export const KAKAO_AUTH_URL = `${BASE_URL}/api/v1/auth/login/kakao?redirect_uri=http://localhost:3000/login`
+export const BASE_URL = process.env.NEXT_PUBLIC_DEV_API_URL;
+export const KAKAO_AUTH_URL = `${BASE_URL}/api/v1/auth/login/kakao?${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI}`
