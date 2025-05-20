9c3851a870264df9251ac4f1b8b9f965455d5873
Jinwoo Kim <70637892+Jinviz@users.noreply.github.com>
2025-05-20 15:49:29 +0900

    Setting: sentry-nextjs 설치 (#46)

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,49 @@
+import { withSentryConfig } from "@sentry/nextjs";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['k.kakaocdn.net', 'example.com', 'images.pexels.com', 'images.unsplash.com', 'i.ibb.co', 'nemo-uploaded-files.s3.ap-northeast-2.amazonaws.com', 'img1.kakaocdn.net', 't1.kakaocdn.net'],
+    domains: [
+      "k.kakaocdn.net",
+      "example.com",
+      "images.pexels.com",
+      "images.unsplash.com",
+      "i.ibb.co",
+      "nemo-uploaded-files.s3.ap-northeast-2.amazonaws.com",
+      "img1.kakaocdn.net",
+      "t1.kakaocdn.net",
+    ],
   },
 };
 
-export default nextConfig;
+export default withSentryConfig(nextConfig, {
+  // For all available options, see:
+  // https://www.npmjs.com/package/@sentry/webpack-plugin#options
+
+  org: "6-nemo",
+  project: "frontend-service",
+
+  // Only print logs for uploading source maps in CI
+  silent: !process.env.CI,
+
+  // For all available options, see:
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+  // Upload a larger set of source maps for prettier stack traces (increases build time)
+  widenClientFileUpload: true,
+
+  // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+  // This can increase your server load as well as your hosting bill.
+  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+  // side errors will fail.
+  // tunnelRoute: "/monitoring",
+
+  // Automatically tree-shake Sentry logger statements to reduce bundle size
+  disableLogger: true,
+
+  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+  // See the following for more information:
+  // https://docs.sentry.io/product/crons/
+  // https://vercel.com/docs/cron-jobs
+  automaticVercelMonitors: true,
+});
---
+++ src/shared/config/sentry/sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://02f5a73f1090858cfbe7e198229fac0d@o4509320439529472.ingest.us.sentry.io/4509324247957504",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
