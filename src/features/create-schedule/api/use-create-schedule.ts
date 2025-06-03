80d56ab2a7f25b0805beae170ff3ac87250ddbda
Jinviz <rlawlsdn9583@gmail.com>
2025-06-04 01:35:07 +0900

    Feat: 일정 참여 응답에 대한 토스트 메세지 출력

--- src/features/create-schedule/api/use-create-schedule.ts
+++ src/features/create-schedule/api/use-create-schedule.ts
@@ -2,6 +2,7 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { post } from "@/features/auth/model/auth-client";
 import { CreateScheduleRequest } from "@/entities/schdule/model/types";
 import { scheduleQuery } from "@/entities/schdule/api/schedule.query";
+import { toast } from "sonner";
 
 export const useCreateSchedule = () => {
     const queryClient = useQueryClient();
@@ -21,6 +22,20 @@ export const useCreateSchedule = () => {
             queryClient.invalidateQueries({
                 queryKey: scheduleQuery.lists(variables.groupId)
             });
+
+            toast.success("일정 참여 응답을 완료하였습니다");
         },
+
+        onError: (error) => {
+            toast.error(
+                error instanceof Error
+                    ? error.message
+                    : "일정 생성에 실패했습니다.",
+                {
+                    description: "잠시 후 다시 시도해주세요.",
+                    position: "top-center",
+                },
+            );
+        }
     });
 };
