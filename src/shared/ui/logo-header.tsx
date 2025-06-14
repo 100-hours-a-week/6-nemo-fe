49572d489ae103b82ab559f575ebdd90b619b884
Ray <rlawlsdn9583@gmail.com>
2025-06-14 23:05:55 +0900

    Feat: 헤더 로고 클릭 시 홈으로 이동 추가

--- src/shared/ui/logo-header.tsx
+++ src/shared/ui/logo-header.tsx
@@ -5,13 +5,15 @@ import { bell_bk_icon, nemo_logo } from "../assets/images";
 export default function LogoHeader() {
   return (
     <header className="flex justify-between px-6 pt-6 pb-4">
-      <Image
-        src={nemo_logo}
-        alt="logo"
-        width={64}
-        height={64}
-        className="object-contain"
-      />
+      <Link href="/">
+        <Image
+          src={nemo_logo}
+          alt="logo"
+          width={64}
+          height={64}
+          className="object-contain"
+        />
+      </Link>
       <Link href={`/notifications`}>
         <Image
           src={bell_bk_icon}
