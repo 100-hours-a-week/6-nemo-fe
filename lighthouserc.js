8db8688fde0545f146250e5965440495d6350399
Jinviz <rlawlsdn9583@gmail.com>
2025-07-08 11:15:51 +0900

    Fix: Lighthouse CI 워크플로우 및 설정파일 수정

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,38 +1,23 @@
 module.exports = {
     ci: {
         collect: {
-            staticDistDir: "./.next",
+            // staticDistDir: "./.next",
+
+            // 서버 시작 방식
             startServerCommand: "pnpm start",
-            startServerReadyPattern: "Ready on",
+            startServerReadyPattern: "Ready in",
             startServerReadyTimeout: 30000,
             url: [
                 'http://localhost:3000/groups',
-                'http://localhost:3000/login',
-                'http://localhost:3000/my-nemo?tab=group',
-                'http://localhost:3000/my-nemo?tab=schedule',
-                'http://localhost:3000/my-profile',
+                // 'http://localhost:3000/login',
+                // 'http://localhost:3000/my-nemo?tab=group',
+                // 'http://localhost:3000/my-nemo?tab=schedule',
+                // 'http://localhost:3000/my-profile',
             ],
             numberOfRuns: 3, // 각 URL당 3번 실행하여 평균값 계산
             settings: {
                 preset: 'desktop',
             },
-        },
-        assert: {
-            assertions: {
-                'categories:performance': ['warn', { minScore: 0.9 }],
-                'categories:accessibility': ['warn', { minScore: 0.9 }],
-                'categories:best-practices': ['warn', { minScore: 0.9 }],
-                'categories:seo': ['warn', { minScore: 0.9 }],
-                // 'categories:pwa': ['warn', { minScore: 0.5 }],
-
-                'first-contentful-paint': ['warn', { maxNumericValue: 2000 }],
-                'largest-contentful-paint': ['warn', { maxNumericValue: 2500 }],
-                'cumulative-layout-shift': ['warn', { maxNumericValue: 0.1 }],
-                'total-blocking-time': ['warn', { maxNumericValue: 300 }],
-                'speed-index': ['warn', { maxNumericValue: 3000 }],
-            },
-
-            preset: 'lighthouse:no-pwa'
         },
         // Github 임시 저장소
         // upload: {
