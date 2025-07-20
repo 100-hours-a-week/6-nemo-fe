44e16855edcdef0ecd9a0b5fc9a19ff460733c27
Jinviz <rlawlsdn9583@gmail.com>
2025-07-20 20:56:50 +0900

    Design: 로그인 페이지 온보딩 콘텐츠 추가

--- src/features/auth/login/ui/login-button.tsx
+++ src/features/auth/login/ui/login-button.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import { kakao_logo } from "@/shared/assets/images";
-import { KAKAO_AUTH_URL } from "@/shared/constants";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { kakao_logo } from "@/shared/assets/images";
+import { KAKAO_AUTH_URL } from "@/shared/constants";
 
 const LoginButton = () => {
   const router = useRouter();
@@ -14,11 +14,11 @@ const LoginButton = () => {
 
   return (
     <button
-      className="bg-kakao flex w-full items-center rounded-xl px-7 py-4"
+      className="bg-kakao flex max-h-[48px] w-full items-center rounded-xl px-7 py-4"
       onClick={handleLogin}
     >
       <Image src={kakao_logo} alt="kakao logo" width={24} height={24} />
-      <span className="text-common-0 mx-auto text-[18px] font-semibold">
+      <span className="text-common-0 text-body-2 mx-auto font-semibold">
         카카오로 3초 만에 시작하기
       </span>
     </button>
